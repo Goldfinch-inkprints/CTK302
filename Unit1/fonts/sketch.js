8282ac526bebabd6c35f4ea1eefaedbfb18cfd1d
Goldfinch-inkprints <artistic_hilly123@yahoo.com>
2021-09-02 10:00:26 -0500

    new font

--- Unit1/fonts/sketch.js
+++ Unit1/fonts/sketch.js
@@ -1,12 +1,18 @@
-var font1;
+var font1, font2;
 
 function setup() {
-  createCanvas(500, 500);
-  font1=loadFont("assets/RoseVelt.ttf")
+  createCanvas(1920, 1080);
+  font1=loadFont("assets/RoseVelt.ttf");
+  font2 = loadFont("assets/ROMANUS.otf");
 }
 
 function draw() {
 background("white");
 textFont(font1, 48);
 text("hello world",100,100);
+
+textFont(font2, 30);
+
+text("Rome wasn't built in a day.",100,250);
+
 }

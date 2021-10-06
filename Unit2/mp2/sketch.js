5df1f7f6659772967e60420838ef3b783df8a7bc
Goldfinch-inkprints <artistic_hilly123@yahoo.com>
2021-10-05 21:06:03 -0500

    Oh high mark!

--- Unit2/mp2/sketch.js
+++ Unit2/mp2/sketch.js
@@ -1,17 +1,26 @@
 let state = 0;
 let timer = 0;
+let y = 1000;
+let x = 200;
 let mic;
 let vol;
-let cake;
+let leaf, state0, state1, Bigpapa;
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(800, 750);
+textSize(20);
+textAlign (CENTER);
+rectMode (CENTER);
+mic = new p5.AudioIn();
+mic.start();
+leaf = loadImage("assets/leaf.png") ;
+state0 = loadImage("assets/state0.png") ;
+state1 = loadImage("assets/state1.png") ;
+Bigpapa = loadImage("assets/Bigpapa.png") ;
+strokeWeight(10);
+textSize(25);
 
-  // code for initializing mic in.
-  mic = new p5.AudioIn(); // what does "new" mean?
-  mic.start();
 
-  cake = loadImage("assets/frog.jpg");
 }
 
 function draw() {
@@ -19,39 +28,71 @@ function draw() {
 
   switch (state) {
     case 0:
-      background('black');
-      image(cake, 200, 200, 150, 150);
-      text("welcome, please click on the cake", 100, 100);
-      break;
+  background('pink');
+  text("Click to begin!", 400, 750/2);
+  text ("Let's help Mark reach his lunch!", 400, 750/2+50);
+  break;
+case 1:
+  background ('lightblue');
+  text("Mark is very hungry but he know \nthe best leaves are at the top!\nWait for it.. \nMark has to find the tallest tree", 550, 200);
+  image(state0, 200,75);
+  timer++;
+  if(timer > 3 * 60){
+    timer = 0;
 
-    case 1:
-      background('yellow');
-      text("listening", 100, 100);
-      // get the sound input
-      vol = (mic.getLevel().toFixed(2)); // returned level is between 0 and 1
-      if (vol > .1) {
-        state = 2 ;
-      }
-      break;
+    state++;
+    if(state > 1){
+      state = 2;
+    }
+  }  break;
 
-    case 2:
-      background('red');
-      text("state 2", 100, 100);
-      timer++;
-      if(timer > 5*60){
-        timer=0;
-        state =0;
-      }
-      break;
-
+case 2:
+  background('lightblue');
+  text("Cheer for Mark to help him reach!", 350, 750/2);
+    vol = (mic.getLevel().toFixed(2));
+    image(Bigpapa, x,y);
+  if (vol > .001) {
+    y = y + -5;
+    if (y < -400){
+      state = state + 1;
+      y = 800;
+    }
   }
 
-  fill('white');
-  text(mouseX + ", " + mouseY, 15, 15);
+  break;
+
+case 3:
+  background ("lightgreen");
+  text("Oh high Mark!", 350, 750/2);
+    vol = (mic.getLevel().toFixed(2));
+    image(Bigpapa, x,y);
+  if (vol > .01) {
+    y = y + -5;
+    if (y < -400){
+      state = state + 1;
+      y = 0;
+    }
+  }
+
+  break;
+
+case 4:
+background ("lightgreen");
+text ("You helped Mark get a tasty lunch! \nGreat Job!", 350, 150);
+image(leaf, 250,200);
+
+break;
+
+}
+
+
+
+  //fill('white');
+  //text(mouseX + ", " + mouseY, 100, 100);
 }
 
 function mouseReleased() {
-  if ((state == 0) && (mouseX > 200) && (mouseX < 350) && (mouseY > 200) && (mouseY < 350)) {
+  if ((state == 0) && (mouseX > 10) && (mouseX < 800) && (mouseY > 100) && (mouseY < 800)) {
     state = 1;
 
   }
